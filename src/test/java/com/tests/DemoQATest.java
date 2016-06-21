161cc9b40e87670310f0816b3aeed98f9a6dd827
Sushrut Naik <sushrutn@nitorinfotech.com>
2016-06-21 17:35:23 +0530

    Added Comment in Test Methods

--- src/test/java/com/tests/DemoQATest.java
+++ src/test/java/com/tests/DemoQATest.java
@@ -11,16 +11,19 @@ public class DemoQATest {
 	private WebDriver driver;		
 	@Test				
 	public void testEasy() {	
+		//launch application
 		driver.get("http://www.guru99.com/selenium-tutorial.html");  
 		String title = driver.getTitle();				 
 		Assert.assertTrue(title.contains("Free Selenium Tutorials")); 		
 	}	
 	@BeforeTest
-	public void beforeTest() {	
+	public void beforeTest() {
+		//initiate driver
 	    driver = new FirefoxDriver();  
 	}		
 	@AfterTest
 	public void afterTest() {
+		//shutdown driver
 		driver.quit();			
 	}
 }

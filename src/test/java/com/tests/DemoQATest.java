7021be8e98e35c457030b25d321af233ff772ec8
Sushrut Naik <sushrutn@nitorinfotech.com>
2016-06-27 17:26:11 +0530

    Added space

--- src/test/java/com/tests/DemoQATest.java
+++ src/test/java/com/tests/DemoQATest.java
@@ -24,6 +24,7 @@ public class DemoQATest {
 		String title = driver.getTitle();				 
 		Assert.assertTrue(title.contains("FreeSeleniumTutorials")); 		
 	}	
+	
 	@BeforeTest
 	public void beforeTest() {
 		//initiate driver
